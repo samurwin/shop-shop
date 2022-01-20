580d7ee0ecb8eae5de9a9ecfb375b50251763d01
smudesign <urwinsamantha@gmail.com>
2022-01-19 19:24:08 -0500

    cart toggles

---
+++ client/src/components/Cart/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import { useStoreContext } from '../../utils/GlobalState';
+import { TOGGLE_CART } from '../../utils/actions';
+
+import CartItem from '../CartItem';
+import Auth from '../../utils/auth';
+import './style.css';
+
+function Cart() {
+    const [state, dispatch] = useStoreContext();
+
+    function toggleCart() {
+        dispatch({ type: TOGGLE_CART });
+    }
+
+    if (!state.cartOpen) {
+        return (
+            <div className='cart-closed' onClick={toggleCart}>
+                <span
+                role='img'
+                aria-label='cart'>🛒</span>
+            </div>
+        );
+    }
+
+    return (
+        <div className='cart'>
+            <div className='close' onClick={toggleCart}>[close]</div>
+            <h2>Shopping Cart</h2>
+            <div>
+                <CartItem item={{name:'Camera', image:'camera.jpg', price:5, purchaseQuantity:3}} />
+                <CartItem item={{name:'Soap', image:'soap.jpg', price:6, purchaseQuantity:4}} />
+
+                <div className='flex-row space-between'>
+                    <strong>Total: $0</strong>
+                    {
+                        Auth.loggedIn() ?
+                        <button>
+                            Checkout
+                        </button>
+                        :
+                        <span>(log in to check out)</span>
+                    }
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Cart;
--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -7,6 +7,8 @@ import spinner from '../assets/spinner.gif';
 import { useStoreContext } from '../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../utils/actions';
 
+import Cart from '../components/Cart';
+
 function Detail() {
   const [state, dispatch] = useStoreContext();
   const { id } = useParams();
@@ -51,6 +53,7 @@ function Detail() {
         </div>
       ) : null}
       {loading ? <img src={spinner} alt="loading" /> : null}
+      <Cart />
     </>
   );
 }
